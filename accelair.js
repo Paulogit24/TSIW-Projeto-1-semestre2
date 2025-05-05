ac3def9265ff086af5ee939a3c764db4d69c11be
Mariana <marianalbferreira27@gmail.com>
2025-05-05 17:54:29 +0100

    Atualização menu

---
+++ accelair.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {
+    scrollFunction();
+  };
+  
+  function scrollFunction() {
+    const navbar = document.getElementById("navbar");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      navbar.style.top = "0";
+    } else {
+      navbar.style.top = "-70px";
+    }
+  }
+  

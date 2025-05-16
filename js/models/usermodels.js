84d972e172a3162063094d8e7c621f938238c87d
Paulogit24 <40240257@esmad.ipp.pt>
2025-05-16 11:07:54 +0100

    added some functions to modelusers

--- js/models/usermodels.js
+++ js/models/usermodels.js
@@ -3,3 +3,25 @@ let Users;
 export function init() {
   users = localStorage.users ? JSON.parse(localStorage.users) : [];
 }
+
+// função para adicinar uum utilizador
+
+
+// classa do utilizador
+class User {
+  username = "";
+  password = "";
+  tier = "";
+  exp = ""
+  banner = "";
+  userimage = "";
+
+  constructor(username, password) {
+    this.username = username;
+    this.password = password;
+    this.tier = "tierless";
+    this.exp = 0;
+    this.banner = "#000000";
+    this.userimage = "#000000";
+  }
+}

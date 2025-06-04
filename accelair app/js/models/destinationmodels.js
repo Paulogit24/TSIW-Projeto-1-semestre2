dc5b6311ad23d2abf86940a5ffb92c68896b1c95
Paulogit24 <40240257@esmad.ipp.pt>
2025-06-04 14:49:30 +0100

    update to DestinationModel

--- accelair app/js/models/destinationmodels.js
+++ accelair app/js/models/destinationmodels.js
@@ -1,67 +1,67 @@
-//model para os destinos da app
-let Destinations;
+//model para os destinos da app   verificar se o nome das variaveis nas funções está correto
+let destinations;
 
 // CARREGAR destinoS DA LOCALSTORAGE
 export function init() {
-  Destination = localStorage.Destination ? JSON.parse(localStorage.Destination) : [];
+  destination = localStorage.destination ? JSON.parse(localStorage.destination) : [];
 }
 
 // ADICIONAR destino  (mudar para try/catch para evitar erros)
-export function addfligth(DestinationNumber, origin, destination, departure, arrival, airline, price) {
-  if (Destination.some((Des) => Des.DestinationNumber === DestinationNumber)) {
-    throw Error(`Des with number "${DestinationNumber}" already exists!`);
+export function addDestination(destinationNumber, origin, destination, departure, arrival, airline, price) {
+  if (destination.some((destination) => destination.destinationNumber === destinationNumber)) {
+    throw Error(`destination with number "${destinationNumber}" already exists!`);
   } else {
-    Destination.push(new Des(DestinationNumber, origin, destination, departure, arrival, airline, price));
-    localStorage.setItem("Destination", JSON.stringify(Destination));
+    destination.push(new destination(destinationNumber, origin, destination, departure, arrival, airline, price));
+    localStorage.setItem("destination", JSON.stringify(destination));
   }
 }
 
 // REMOVER destino
-export function removeDes(DestinationNumber) {
-  Destination = Destination.filter((Des) => Des.DestinationNumber !== DestinationNumber);
-  localStorage.setItem("Destination", JSON.stringify(Destination));
+export function removedestination(destinationNumber) {
+  destination = destination.filter((destination) => destination.destinationNumber !== destinationNumber);
+  localStorage.setItem("destination", JSON.stringify(destination));
 }
 
 // DEFINIR O destino ATUAL (AQUELE QUE SERÁ VISTO NO DETALHE)
-export function setCurrentDes(DestinationNumber) {
-  localStorage.setItem("Des", DestinationNumber);
+export function setCurrentdestination(destinationNumber) {
+  localStorage.setItem("destination", destinationNumber);
 }
 
 // OBTER O destino ATUAL (OBJETO COMPLETO)
-export function getCurrentDes() {
-  return Destination.find((Des) => Des.DestinationNumber === localStorage.getItem("Des"));
+export function getCurrentdestination() {
+  return destination.find((destination) => destination.destinationNumber === localStorage.getItem("destination"));
 }
 
 // ORDENAR destinoS POR PREÇO OU DATA DE PARTIDA
-export function sortDestination(by = "price") {
+export function sortdestination(by = "price") {
   if (by === "price") {
-    Destination.sort((a, b) => a.price - b.price);
+    destination.sort((a, b) => a.price - b.price);
   } else if (by === "departure") {
-    Destination.sort((a, b) => new Date(a.departure) - new Date(b.departure));
+    destination.sort((a, b) => new Date(a.departure) - new Date(b.departure));
   }
 }
 
 // OBTER destinoS (COM FILTROS E ORDENAÇÃO)
-export function getDestination(filterOrigin = "", filterDestination = "", isSorted = false, sortBy = "price") {
-  let filteredDestination = Destination.filter(
-    (Des) =>
-      (Des.origin.toLowerCase().includes(filterOrigin.toLowerCase()) || filterOrigin === "") &&
-      (Des.destination.toLowerCase().includes(filterDestination.toLowerCase()) || filterDestination === "")
+export function getdestination(filterOrigin = "", filterdestination = "", isSorted = false, sortBy = "price") {
+  let filtereddestination = destination.filter(
+    (destination) =>
+      (destination.origin.toLowerCase().includestination(filterOrigin.toLowerCase()) || filterOrigin === "") &&
+      (destination.destination.toLowerCase().includestination(filterdestination.toLowerCase()) || filterdestination === "")
   );
 
-  filteredDestination = isSorted
-    ? filteredDestination.sort((a, b) => sortBy === "price"
+  filtereddestination = isSorted
+    ? filtereddestination.sort((a, b) => sortBy === "price"
         ? a.price - b.price
         : new Date(a.departure) - new Date(b.departure))
-    : filteredDestination;
+    : filtereddestination;
 
-  return filteredDestination;
+  return filtereddestination;
 }
 
-class Destination {
-  constructor(name, description, image, location, price, category) {
+class destination {
+  constructor(name, destinationcription, image, location, price, category) {
     this.name = name;
-    this.description = description;
+    this.destinationcription = destinationcription;
     this.image = image;
     this.location = location;
     this.price = price;

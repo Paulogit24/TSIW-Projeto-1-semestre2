4d5429dad68a6cbc5134e1aeee25a7c21f2f2439
Mariana <marianalbferreira27@gmail.com>
2025-06-07 19:42:55 +0100

    Atualização index

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function () {
-  const navbar = document.querySelector("#navbar nav");
-
-  if (window.scrollY > 100) {
-    navbar.classList.add("navbar-branco");
-  } else {
-    navbar.classList.remove("navbar-branco");
-  }
-});
